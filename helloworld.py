f80e12881e8d7921f8f50c045988747af21ce4c1
shirin197 <nadina@wolke7.net>
2021-03-05 21:38:52 +0100

    i lul eig nix

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,25 @@
+"""
+@Nadina Amsler (shirin197)
+@2021-05-03
+"""
 # Hello World
 print("Hello world")
 
 # Variable
-timischscheisse = 1
+a = 1
 
 # if abfrage
-if timischscheisse == 1:
-    print("tim isch scheisse is true")
+if a == 1:
+    print("a is true")
+
+# Addition von zwei Zahlen
+nummer1 = 2
+nummer2 = 4
+
+summe = nummer1 + nummer2
+
+# Ausgabe der Variable Summe
+print(summe)
+
+
+
