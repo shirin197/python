ab0cb44182bfc6187074d94708b766d374d26fa0
shirin197 <nadina@wolke7.net>
2021-04-10 23:02:29 +0200

    numbers

--- learning.py
+++ learning.py
@@ -4,9 +4,9 @@
 """
 # Hello World
 print("Hello world")
-print("\n") #line break
+print("\n") # line break
 
-#Drawing a triangle
+# Drawing a triangle
 print("   /|")
 print("  / |")
 print(" /  |")
@@ -18,14 +18,14 @@ print("\n")
 # separading word for a variable with _
 # for a number i dont need "", only when i make a String
 
-person_name = "Lynn" #string
+person_name = "Lynn" # string
 person_age = "20"
 person_country = "Italy"
-is_Female = True #boolean
+is_Female = True # boolean
 
 print("Hello there, my name is " + person_name + ", ")
 print("i am " + person_age + " and from " + person_country + " ")
-#its possible to change the name in the half of the story, i updated my variable inside of the programm
+# its possible to change the name in the half of the story, i updated my variable inside of the programm
 person_name = "Phoebe"
 print("I really like my name " + person_name + ", ")
 print("but i didn't like being " + person_age + ".")
@@ -43,9 +43,9 @@ print("\n")
 
 #Functions
 word = "Python"
-       #012345 -> positions in the string
-       # word = "Python is nice" is also a string
-       # list of functions -> https://www.w3schools.com/python/python_ref_functions.asp
+       # 012345 -> positions in the string
+# word = "Python is nice" is also a string
+# list of functions -> https://www.w3schools.com/python/python_ref_functions.asp
 
 print(word.lower()) #lower put all letters in Lower case letters
 print(word.upper()) #upper put all letters in UPPER case letters
@@ -56,6 +56,21 @@ print(word[0]) #print(word[index of the charakter i want]) string start wth 0
 print(word.index("h")) # retun us the index of our specific letter(capitol) inside of the string
 print(word.index("thon")) # shows me where this starts (here 2 bc. thats where "thon" starts
 print(word.replace("Python", "Java")) # replace specific words in the string
+
+print("\n")
+
+#Numbers
+print(1)
+print(1.908)
+print(-2)
+print(1 + 8) # all types of calculations are possible
+print(5 * (3 + 6))
+print(10 % 3)
+my_num = 5
+print(my_num)
+my_num =5
+print(str(my_num) + " my fav. number")
+# my_num will be convertet into a string -> useful when u want to print out a number next to a string
 
 print("\n")
 
