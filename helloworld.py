ad3347c275fa82c13478bf1c08112f726c003e8f
shirin197 <nadina@wolke7.net>
2021-04-10 10:55:34 +0200

    changes

--- helloworld.py
+++ helloworld.py
@@ -4,13 +4,38 @@
 """
 # Hello World
 print("Hello world")
+print("\n") #line break
+
+#Drawing a triangle
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+print("\n")
 
 # Variable
-a = 1
+# separading word for a variable with _
+# for a number i dont need "", only when i make a String
 
+person_name = "Lynn"
+person_age = 20
+person_country = "Italy"
+is_Female = True #boolean
+
+print("Hello there, my name is " + person_name + ", ")
+print("i am " + person_age + " and from " + person_country + " ")
+#its possible to change the name in the half of the story, i updated my variable insid eof the programm
+person_name = "Phoebe"
+print("I really like my name " + person_name + ", ")
+print("but i didn't like being " + person_age + ".")
+
+print("\n")
 # if abfrage
+a = 1
 if a == 1:
     print("a is true")
+print("\n")
 
 # Addition von zwei Zahlen
 nummer1 = 2
@@ -20,6 +45,7 @@ summe = nummer1 + nummer2
 
 # Ausgabe der Variable Summe
 print(summe)
+
 
 
 
