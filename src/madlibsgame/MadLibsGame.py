a1b9743e92244aaa218ff4f7fc6865ad8d527b9f
shirin197 <nadina@wolke7.net>
2021-04-12 22:50:00 +0200

    python

---
+++ src/madlibsgame/MadLibsGame.py
@@ -0,0 +1,34 @@
+"""
+@Nadina Shirin Amsler (shirin197)
+@2021-12-04
+@Mad Libs Game is a game were u can add a bunch of random words into a story
+"""
+
+#Variables
+adjective = input("Enter a adjective: ")
+noun = input("Enter a noun: ")
+plural_noun = input("Enter a plural noun: ")
+female_name = input("Enter a female_name: ")
+adjective2 = input("Enter a second adjective: ")
+article_of_clothing = input("Enter a article of clothing: ")
+noun2 = input("Enter a second noun: ")
+a_city = input("Enter a city: ")
+plural_noun2 = input("Enter a second plural noun: ")
+bodypart = input("Enter a bodypart: ")
+one_letter = input("Enter a letter: ")
+celebrity = input("Enter a celebrity name: ")
+one_letter = input("Enter a adjective: ")
+plural_noun3 = input("Enter a third plural noun: ")
+
+print("\n")
+
+#story out print
+print("There are many " + adjective + " ways to choose a/an " + noun + " to")
+print("read. First you could ask for recommendations form your friends and")
+print(plural_noun + ". Just don't ask Aunt " + female_name + " she only")
+print("reads " + adjective2  + " books with " + article_of_clothing + " -ripping goddesses")
+print("on the cover. If your friends and family are no help, try checking out the ")
+print(noun2 + " Review in The " + a_city + "Times. If the " + plural_noun2)
+print("featured there are too {adjective3} for your taste, try something a little")
+print("more low- " + bodypart + ", like " + one_letter + ": The " + celebrity)
+print("Magazine, or " + plural_noun3 + " Magazine.")
