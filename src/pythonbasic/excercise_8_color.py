dca695fe848509843267a8f988a3972672abaa8c
shirin197 <73479005+shirin197@users.noreply.github.com>
2021-07-12 11:21:56 +0200

    ok

--- src/pythonbasic/excercise_8_color.py
+++ src/pythonbasic/excercise_8_color.py
@@ -1,2 +1,8 @@
 color_list = ["Red","Green","White" ,"Black"]
+# [-1] from 0 back to the last one
 print((color_list[0],color_list[-1]))
+
+color_list_two = ["Black", "Yellow", "Blue", "Violett"]
+#                   [0]       [1]       [3]     [4]
+#                   [-4]      [-3]      [-2]    [-1]
+print((color_list_two[0], color_list_two[-3]))
---
+++ src/pythonbasic/excercise_9_examdate.py
@@ -0,0 +1,3 @@
+# Tupel         0   1    2
+exam_st_date = (11, 12, 2014)
+print( "The examination will start from : %i / %i / %i"%exam_st_date)
