fc111a8926826f9c3dee93c5fc189d1fce3adff3
shirin197 <nadina@wolke7.net>
2021-11-26 21:12:29 +0100

    ok

---
+++ src/pythonbasic/excercise_8_colorlist.py
@@ -0,0 +1,4 @@
+# array color list
+color_list = ["Red","Green","White" ,"Black"]
+# %S allows me to insert, in this case a string -> it will be replaced by what i pass to the string
+print("%s %s"%(color_list[0], color_list[3]))
---
+++ src/pythonbasic/excercise_9_examination_schedule.py
@@ -0,0 +1,3 @@
+
+exam_st_date = (11, 12, 2021)
+print("The examination will start from : %d / %d / %d" % exam_st_date)
